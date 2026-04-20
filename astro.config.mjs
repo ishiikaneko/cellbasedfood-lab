8c9f09caf667e8b38c4fe86e8ae01ca04cc389f6
ishiikaneko <ishiikaneko1317@gmail.com>
2026-04-21 00:08:14 +0900

    fix: remove sitemap for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cellbasedfood-ishiikaneko.com',
-  integrations: [sitemap()],
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light',
-    },
-  },
+  site: 'https://cellbasedfood-lab.vercel.app',
 });
